10024624412465e067163033f111c61fccacf630
dhanusherramuri <dhanush.erramuri.raj@gmail.com>
2026-08-06 10:38:28 +0530

    Commit

--- First.py
+++ First.py
@@ -1,1 +1,13 @@
-print ("Hello")
+
+
+def add(x, y):
+    return x+y
+
+def next_nop(x , y):
+    if x < 10 :
+        print( "X is less than 10" )
+    else :
+        g(x , y)
+
+print( next_nop(8, 20))
+print(add(4,5))
