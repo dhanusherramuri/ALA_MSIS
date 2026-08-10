1fddadbbd7e469b331b5961b28e56c292d31689c
dhanusherramuri <dhanush.erramuri.raj@gmail.com>
2026-08-10 12:31:15 +0530

    Latest Commit

--- tx.py
+++ tx.py
@@ -1,12 +1,18 @@
 class UpiTx():
     def __init__(self, sender, receiver, amount):
+
+        #create a UUID Instance (python has a inbuilt library)
+        self.tx_id = ?
+        self.sender = sender
+        self.receiver = receiver
         pass
 
 class UpiPaymentTx(UpiTx):
-    def __init__(sender_handle, receiver_handle, amount):
+    def __init__(self,sender_handle, receiver_handle, amount):
+        #pass the responsibility to 'UpiTX'
         pass
 
 class UpiReceiptTx(UpiTx):
-    def __init__(sender_handle, receiver_handle, amount):
+    def __init__(self,sender_handle, receiver_handle, amount):
         pass
         
