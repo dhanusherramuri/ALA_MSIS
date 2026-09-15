fb9cff634ed07b5d8a81b78977f3e670805398e1
dhanusherramuri <dhanush.erramuri.raj@gmail.com>
2026-09-15 10:41:05 +0530

    UNIT TEST CASES HAVE BEEN IMPLEMENTED AND DOCUMENTATION HAS BEEN WRITTEN

--- Assignment/CODES/test_vector.py
+++ Assignment/CODES/test_vector.py
@@ -4,31 +4,35 @@ import math
 if __name__ == "__main__" :
     v1 = Vec((1,2,3,4,5))
 
-    # print(f"Vector 1 : ",v1)
-
     m = Vec.mean(v1)
-    # print(f"MEAN : ",m)
+    assert m == 3
 
-    # mv = (m,) * len(v1)
     dmean = Vec.demean(v1)
-    # print(f"\nDEMEAN OF VECTOR V1: ",dmean)
-    # print (f"\nMEAN OF DEMEAN VECTOR V1 : ",Vec.mean(dmean))
     assert abs(dmean.mean() ) < 1e-9
 
     v2 = Vec((7,7,7,7))
     edm = Vec.demean(v2)
-    # print(f"\nDEMEAN OF VECTOR V2: ",edm)
-    # print (f"MEAN OF DEMEAN VECTOR V2 : ",Vec.mean(edm))
     assert (abs (edm.mean())) < 1e-9 
 
     v3 = Vec((10,20,30,40,50))
     std1 = Vec.std(v3)
-    # print(f"\nSTANDARD DEVIATION : ",std)
     assert abs(std1 > 0)
 
     std2 = Vec.std(v2)
-    # print(f"\nSTANDARD DEVIATION OF V2 : ",std)
     assert abs(std2)< 1e-9
 
     print(f"ALL ASSERTIONS PASSED")
-    # v4 = Vec(())
+
+
+'''DOCUMENTATION FOR THE CODE
+(1) 'm' gets us the mean of the vector v1 
+(2) 'dmean' variable stores the demeaned vector and the assert statement makes sure that the 
+MEAN OF DEMEANED VECTOR is 0 and 1e - 9 has been used over here because dmean returns the result
+in floating point
+(3) 'edm' variable refers to equal demean which means when all the elements passed are equal and
+the assert is used to check if the value retured is 0 because same values always returns 0
+(4) 'std1' is used to check if the standard deviation of the vector consisting of different elements
+returns a value which is greater than 0 because distinct elements have a standard deviation greater than 0
+(5) 'std2' is used to check the value of the vector 'v2' which has all the same elements so as to check if 
+the property which states that the standard deviation will be 0 for the same vector elements satisfies or not.
+    '''
